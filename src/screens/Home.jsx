338af61d32ace0f8905d12a78011043b870dcde6
Fabiola Zambrana Soria <FZambrana@SENASIR.local>
2025-01-13 14:07:35 -0400

    Manage props

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,13 +1,23 @@
-import Logo from '../Components/Home/Logo.jsx';
+//import Logo from '../Components/Home/Logo.jsx';
+//import GitHub from '../Components/Home/Github.jsx'
+
+import OpenLink from "../Components/Core/OpenLink.jsx";
+
 import Body from '../Components/Home/Body.jsx';
 import Footer from '../Components/Home/Footer.jsx';
-import GitHub from '../Components/Home/Github.jsx'
+
+import reactLogo from '../assets/react.svg';
+import viteLogo from '../assets/vite.svg';
+import githubLogo from '../assets/GitHub.svg';
 
 const Home = () =>{
     return (
         <div>
-            <Logo />
-            <GitHub />
+           {/* <Logo />
+            <GitHub />*/}
+            <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo} />
+            <OpenLink redirectURL="https://react.dev" logoImage={reactLogo} />
+            <OpenLink redirectURL="https://github.com/fabisoria86/desarrollo-frontend-react-2025" logoImage={githubLogo} />
             <Body />
             <Footer />
         </div>
