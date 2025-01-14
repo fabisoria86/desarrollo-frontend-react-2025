cdf1e0add1bfe77218cfb564dbfcb2ca310380d0
Fabiola Zambrana Soria <FZambrana@SENASIR.local>
2025-01-14 11:47:56 -0400

    laboratorio 6

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -10,9 +10,14 @@ import reactLogo from '../assets/react.svg';
 import viteLogo from '../assets/vite.svg';
 import githubLogo from '../assets/GitHub.svg';
 
+import { useSelector } from 'react-redux'
+
+
 const Home = () =>{
+    const count = useSelector((state) => state.counter.value)
     return (
         <div>
+            <div><h2><strong>Count value: {count}</strong></h2></div>
             <div>
             <OpenLink redirectURL="https://vite.dev" logoImage={viteLogo} />
             <OpenLink redirectURL="https://react.dev" logoImage={reactLogo} />
