ceb36e0c9a7b76c59cafc6dbf4f07e1045ab0dc7
Fabiola Zambrana Soria <FZambrana@SENASIR.local>
2025-01-15 11:58:00 -0400

    Añadimos rutas

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 // import Default from './Components/Default.jsx'
-//import Logo from './Components/Home/Logo.jsx';
-//import Body from './Components/Home/Body.jsx';
-//import Footer from './Components/Home/Footer.jsx';
-import Home from './screens/Home.jsx';
-import Counter from './Components/Counter/Counter.jsx';
-import Product from './screens/Product.jsx';
+// import Logo from './Components/Home/Logo.jsx';
+// import Body from './Components/Home/Body.jsx';
+// import Footer from './Components/Home/Footer.jsx';
+//import Home from './screens/Home.jsx';
+//import Counter from './Components/Counter/Counter.jsx';
+//import Product from './screens/Product.jsx';
+import Navigator from './Components/Navigation/Navigator.jsx'
+import Router from './Components/Navigation/Router.jsx'
 
 function App() {
   return (
@@ -13,11 +15,13 @@ function App() {
       {/*<Default/>
       <Logo></Logo>
       <Body></Body>
-      <Footer></Footer>*/}
+      <Footer></Footer>
       <Home />
       <Counter />
       <br />
-      <Product />
+      <Product />*/}
+      <Navigator />
+      <Router />
     </>
   )
 }
---
+++ src/screens/Think.jsx
@@ -0,0 +1,9 @@
+const Think = () => {
+    return (
+        <>
+            <p>Component About</p>
+        </>
+    );
+}
+
+export default Think;
